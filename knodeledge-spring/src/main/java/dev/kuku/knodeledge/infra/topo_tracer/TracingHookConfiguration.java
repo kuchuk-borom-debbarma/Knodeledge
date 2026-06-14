cfa0310e9886d30c5b1c1f2a85858246ee93deda
kuchuk-borom-debbarma <45627511+kuchuk-borom-debbarma@users.noreply.github.com>
2026-06-15 03:25:57 +0530

    simplified trace hook

--- knodeledge-spring/src/main/java/dev/kuku/knodeledge/infra/topo_tracer/TracingHookConfiguration.java
+++ knodeledge-spring/src/main/java/dev/kuku/knodeledge/infra/topo_tracer/TracingHookConfiguration.java
@@ -1,8 +1,6 @@
 package dev.kuku.knodeledge.infra.topo_tracer;
 
 import dev.kuku.topotracer.sdk.LogHook;
-import dev.kuku.topotracer.sdk.TraceHook;
-import dev.kuku.topotracer.sdk.Span;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,28 +11,7 @@ public class TracingHookConfiguration {
 
     @Bean
     public LogHook slf4jLogHook() {
-        return (message, data, importanceLevel) -> {
-            log.info("[Trace Log] {} | data: {} | importance: {}", message, data, importanceLevel);
-        };
-    }
-
-    @Bean
-    public TraceHook slf4jTraceHook() {
-        return new TraceHook() {
-
-            @Override
-            public void onSpanStart(Span span) {
-                log.info("[Span Start] {} | id: {} | traceId: {} | type: {} | importance: {}",
-                    span.getStartMessage(), span.getId(), span.getTraceId(), span.getNodeType(), span.getImportanceLevel());
-            }
-
-            @Override
-            public void onSpanEnd(Span span) {
-                log.info("[Span End] {} | id: {} | traceId: {} | duration: {}ms",
-                    span.getStartMessage(), span.getId(), span.getTraceId(),
-                    (System.currentTimeMillis() - span.getStartedAt()));
-            }
-        };
+        return (message, data, _) -> log.info("[Trace Log] {}, {}", message, data);
     }
 
     @Bean
